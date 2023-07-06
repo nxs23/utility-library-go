de02f9c0cdaaa01045b385f512972b8b30bde343
krisrybak <kris.rybak@hotmail.co.uk>
2023-07-06 14:55:25 +0100

    Adds logging module

---
+++ logging/go.mod
@@ -0,0 +1,5 @@
+module github.com/nxs23/utility-library-go/logging
+
+go 1.20
+
+require github.com/apsdehal/go-logger v0.0.0-20190515212710-b0d6ccfee0e6
---
+++ logging/logger/logger.go
@@ -0,0 +1,89 @@
+package logger
+
+import (
+	"os"
+
+	log "github.com/apsdehal/go-logger"
+)
+
+const (
+	PROD = "prod"
+	DEV  = "dev"
+)
+
+type Logging interface {
+	Fatalf(format string, v ...interface{})
+	Panicf(format string, v ...interface{})
+	Printf(format string, v ...interface{})
+}
+
+type Logger struct {
+	Mode   string
+	Logger *log.Logger
+}
+
+func NewLogger(module string) *Logger {
+	log, _ := log.New(module, 1)
+	log.SetFormat("#%{id} %{time:2006-01-02 15:04:05.123} ▶ [%{module}][%{level}]: %{message}")
+
+	mode := os.Getenv("LOGGER_MODE")
+	if mode != PROD {
+		mode = DEV
+	}
+
+	return &Logger{
+		Mode:   mode,
+		Logger: log,
+	}
+}
+
+func (l *Logger) Fatalf(format string, err ...interface{}) {
+	l.CriticalF(format, err...)
+}
+
+func (l *Logger) Panicf(format string, err ...interface{}) {
+	l.CriticalF(format, err...)
+}
+
+func (l *Logger) Printf(format string, err ...interface{}) {
+	l.DebugF(format, err...)
+}
+
+func (l *Logger) CriticalF(format string, err ...interface{}) {
+	l.Logger.CriticalF(format, err...)
+}
+
+func (l *Logger) WarningF(format string, err ...interface{}) {
+	if l.Mode == DEV {
+		l.Logger.SetLogLevel(log.DebugLevel)
+		l.Logger.WarningF(format, err...)
+	}
+}
+
+func (l *Logger) ErrorF(format string, err ...interface{}) {
+	if l.Mode == DEV {
+		l.Logger.SetLogLevel(log.DebugLevel)
+		l.Logger.ErrorF(format, err...)
+	}
+}
+
+func (l *Logger) NoticeF(format string, err ...interface{}) {
+	if l.Mode == DEV {
+		l.Logger.SetLogLevel(log.DebugLevel)
+		l.Logger.NoticeF(format, err...)
+	}
+}
+
+func (l *Logger) InfoF(format string, err ...interface{}) {
+	if l.Mode == DEV {
+		l.Logger.SetLogLevel(log.DebugLevel)
+		l.Logger.InfoF(format, err...)
+	}
+}
+
+func (l *Logger) DebugF(format string, err ...interface{}) {
+	if l.Mode == DEV {
+		l.Logger.SetLogLevel(log.DebugLevel)
+		l.Logger.DebugF(format, err...)
+	}
+}
